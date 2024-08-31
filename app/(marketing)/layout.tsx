2a8888d0db77d49ae20b63a6843ba58c47406030
Morioka-Ryosuke06 <mrok.ryosuke@gmail.com>
2024-08-31 13:02:33 +0900

    update mainNv

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,13 +2,14 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 import MainNav from '@/components/main-nav';
+import { marketingConfig } from '@/config/marketing';
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <header className="bg-background container z-40">
         <div className="h-20 py-6 flex items-center justify-between">
-          <MainNav></MainNav>
+          <MainNav items={marketingConfig.mainNav}></MainNav>
           <nav>
             <Link href={'/login'} className={cn(buttonVariants({ variant: 'secondary', size: 'sm' }), 'px-4')}>
               ログイン
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,17 +1,24 @@
+import { NavItem } from '@/types';
 import Link from 'next/link';
-export default function MainNav() {
+import { ReactNode } from 'react';
+
+interface MainNavProps {
+  items?: NavItem[];
+  children?: ReactNode;
+}
+
+export default function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex items-center md:gap-10">
       <Link href={'/'} className="hidden md:flex items-center space-x-2">
         <span className="font-bold ">Post Writer</span>
       </Link>
       <nav className="md:flex gap-6 hidden sm:inline-block">
-        <Link href={'/blog'} className="text-lg sm:text-sm font-medium hover:text-foregrond/80">
-          ブログ
-        </Link>
-        <Link href={'/blog'} className="text-lg sm:text-sm font-medium hover:text-foregrond/80">
-          特徴
-        </Link>
+        {items?.map((item, index) => (
+          <Link key={index} href={item.href} className="text-lg sm:text-sm font-medium hover:text-foregrond/80">
+            {item.title}
+          </Link>
+        ))}
       </nav>
     </div>
   );
