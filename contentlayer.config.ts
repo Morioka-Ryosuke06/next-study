267af329c06098e317cd9e15c1db9ec84dee0397
Morioka-Ryosuke06 <mrok.ryosuke@gmail.com>
2024-08-31 22:54:42 +0900

    add contntlayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,36 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `blog/**/*/.mdx`,
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string',
+    },
+    date: {
+      type: 'date',
+      require: true,
+    },
+    published: {
+      type: 'boolean',
+      default: true,
+    },
+    images: {
+      type: 'string',
+      required: true,
+    },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({ contentDirPath: './content', documentTypes: [Post] });
