9f8f1a50f5ffffbcabe8fd8b5ca01e1ddd5474f6
Morioka-Ryosuke06 <mrok.ryosuke@gmail.com>
2024-08-31 18:48:05 +0900

    add footer

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 import MainNav from '@/components/main-nav';
 import { marketingConfig } from '@/config/marketing';
+import SiteFooter from '@/components/site-footer';
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -18,6 +19,7 @@ export default function MarketingLayout({ children }: { children: React.ReactNod
         </div>
       </header>
       <main>{children}</main>
+      <SiteFooter />
     </div>
   );
 }
