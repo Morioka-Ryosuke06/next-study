9b492a32b74b05b6a79e5c49b096b0d8f1b77f44
Morioka-Ryosuke06 <mrok.ryosuke@gmail.com>
2024-08-31 12:31:32 +0900

    add header

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <header className="bg-background container z-40">
+        <div className="h-20 py-6">
+          <nav>
+            <Link href={'/login'} className={cn(buttonVariants({ variant: 'secondary', size: 'sm' }), 'px-4')}>
+              ログイン
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
